2aaf62b6da780e15b2e435677c47bc7c40c9f243
MichaelDulin <michaeldulin1211@gmail.com>
2023-04-11 18:02:37 +0000

    test

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require('dotenv').config();
+const server = require('./src/server');
+const PORT = process.env.PORT;
--- src/server.js
+++ src/server.js
@@ -1,90 +1,22 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
-const bcrypt = require('bcrypt');
 const app = express();
 const cors = require('cors');
-const base64 = require('base-64');
-const { sequelize, DataTypes, Sequelize } = require('sequelize');
+const error500 = require('./middleware/500');
+const error404 = require('./middleware/404');
 
-const PORT = process.env.PORT || 3001;
-const SQL_URL = process.env.SQL_URL || 'sqlite:memory:';
+app.use(cors());
+app.use(express.json());
 
-const sequelize = new Sequelize(SQL_URL);
+app.use(userRouter);
 
-app.use(express.urlencoded({ extended: true }));
+app.use('*', error404);
+app.use(error500);
 
-// Create a Sequelize model
-const Users = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  }
-});
-
-// Signup Route -- create a new user
-// Two ways to test this route with httpie
-// echo '{"username":"john","password":"foo"}' | http post :3000/signup
-// http post :3000/signup username=john password=foo
-app.post('/signup', async (req, res) => {
-
-  try {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    const record = await Users.create(req.body);
-    res.status(200).json(record);
-  } catch (e) { res.status(403).send('Error Creating User'); }
-});
-
-
-// Signin Route -- login with username and password
-// test with httpie
-// http post :3000/signin -a john:foo
-app.post('/signin', async (req, res) => {
-
-  /*
-    req.headers.authorization is : "Basic am9objpmb28="
-    To get username and password from this, take the following steps:
-      - Turn that string into an array by splitting on ' '
-      - Pop off the last value
-      - Decode that encoded string so it returns to user:pass
-      - Split on ':' to turn it into an array
-      - Pull username and password from that array
-  */
-
-  let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'am9objpmb28=']
-  let encodedString = basicHeaderParts.pop();  // am9objpmb28=
-  let decodedString = base64.decode(encodedString); // "username:password"
-  let [username, password] = decodedString.split(':'); // username, password
-
-  /*
-    Now that we finally have username and password, let's see if it's valid
-    1. Find the user in the database by username
-    2. Compare the plaintext password we now have against the encrypted password in the db
-       - bcrypt does this by re-encrypting the plaintext password and comparing THAT
-    3. Either we're valid or we throw an error
-  */
-  try {
-    const user = await Users.findOne({ where: { username: username } });
-    const valid = await bcrypt.compare(password, user.password);
-    if (valid) {
-      res.status(200).json(user);
-    }
-    else {
-      throw new Error('Invalid User');
-    }
-  } catch (error) { res.status(403).send('Invalid Login'); }
-
-});
-
-// make sure our tables are created, start up the HTTP server.
-sequelize.sync()
-  .then(() => {
-    app.listen(3000, () => console.log('server up'));
-  }).catch(e => {
-    console.error('Could not start server', e.message);
-  });
+module.exports = {
+  app,
+  start:  (port) => app.listen(port, () => {
+    console.log(`Server up and listening on ${port}`);
+  }),
+};
